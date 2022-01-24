1cbc077a3ce86c5fe9a2040c468979531e51d164
Matheson94 <mathesonchisholm@hotmail.com>
2022-01-24 21:56:15 +0000

    header partial added

--- express_server.js
+++ express_server.js
@@ -23,6 +23,6 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.b2xVn2	, longURL: 'http://www.lighthouselabs.ca' };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
