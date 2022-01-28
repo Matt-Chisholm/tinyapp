f9b6c20f199a23102f0b39e2a5d71389a55585e3
Matheson94 <mathesonchisholm@hotmail.com>
2022-01-28 19:57:57 +0000

    commenting for helper functions

--- helpers.js
+++ helpers.js
@@ -1,3 +1,4 @@
+// Checking user DB for user by email 
 const getUserByEmail = (email, database) => {
   for (const user in database) {
     if (database[user].email === email) {
@@ -7,6 +8,7 @@ const getUserByEmail = (email, database) => {
   return undefined;
 };
 
+// Random string generator for shortUrls/UserIds
 const generateRandomString = function() {
   let x = (Math.random() + 1).toString(36).substring(6);
   return x;
